bcabc30bea65ae171cce0c664a0851f072b3885d
Ryan Inawat <rrinawat@gmail.com>
2018-01-02 21:10:00 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,13 @@
 //define functions here
-
+//getIt, clicks within paragraph generates alert ("Hey!")
+function getIt(){
+  $('p').on("click", function(){
+    alert("Hey!")
+  })
+}
 $(document).ready(function(){
 
 // call functions here
+getIt()
 
 });
